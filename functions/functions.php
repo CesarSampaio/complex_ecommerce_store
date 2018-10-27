5aa3d7969362da7d18f108f689d1bf8e5f2b6964
CesarSampaio <c.sampaiosan@gmail.com>
2018-10-27 01:15:16 -0400

    Version 1.0.0

--- functions/functions.php
+++ functions/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = mysqli_connect("localhost","root","","ecom_store");
+$db = mysqli_connect("localhost","root","","complex_ecommerce_store");
 
 /// IP address code starts /////
 function getRealUserIp(){
